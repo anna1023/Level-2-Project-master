71ab13cd925111388a6e5f8db9574817160b8569
test <annac101@nycstudents.net>
2023-11-08 00:23:08 -0500

    2

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let list = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 let score = 0;
 let lastFlipped;
+let flippedCards = [];
+let congrats = document.getElementById("win");
+let rules = document.getElementById("rules");
+
 function shuffle (){
     for(let x = 0; x < 500; x++){
         let numberOne = Math.floor(Math.random()*16);
@@ -41,9 +45,13 @@ function display(position){
 function engine(position){
     display(position);
     let card = document.getElementById(position);
+    if(flippedCards.includes(card)){
+        return '';
+    }
     if (lastFlipped != null){
         if (check(card,lastFlipped)){
-            
+           flippedCards.push(card);
+           flippedCards.push(lastFlipped);
         }
         else{
             let last = lastFlipped;
@@ -53,23 +61,32 @@ function engine(position){
         }, 250);
         }
         lastFlipped = null;
+        score++;
+        let scoreDisplay = document.getElementById("score");
+        scoreDisplay.innerHTML = "Score: "+score;
     }
     else {
         lastFlipped = card;
     }
+    win();
 }
 
 function reset(){
+    rules.style.visibility = "hidden";
+    congrats.style.visibility = "hidden";
+    lastFlipped = null;
     for (let x = 0; x<16; x++){
         let card = document.getElementById(x);
         card.setAttribute("src","pokemoncard.png");
     }
     shuffle();
     score = 0;
+    let scoreDisplay = document.getElementById("score");
+        scoreDisplay.innerHTML = "Score: "+score;
 }
 
 function rule(){
-
+    alert("Welcome to Guess The Pokemon! In this game, you'll have to match pairs of pokemon cards with the same image. In this game, you'll have to match pairs of pokemon cards with the same image. Matching all 16 cards will win you the game. If you manage to match a pair, those cards will stay flipped.If you don't manage to match a pair, both cards you select will be flipped back");
 }
 
 function check(cardOne, cardTwo){
@@ -78,4 +95,15 @@ function check(cardOne, cardTwo){
     }
     return false;
 }
+
+function win(){
+    if (flippedCards.length==16){
+        setTimeout(() => {
+            alert("Congrations you win! Scroll down to start again.")
+        }, 250);
+    }
+}
+
+
+
 
